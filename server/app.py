e6d71605119723b7fb8817ee2cfb6424bdc83412
Billy Lowry <billyn.lowry@gmail.com>
2023-06-12 09:30:43 -0400

    completed lab

--- server/app.py
+++ server/app.py
@@ -2,4 +2,18 @@
 
 from flask import Flask
 
-app = Flask(__name__)
+app = Flask(__name__) # equal to the name of the module in question 
+
+
+@app.route('/') # registers the function with the Flask application instance app
+#@app.route() decorator - an instance method that modifies app, creating a rule that requests for the base URL (/) should show our index.
+def index():
+    return '<h1>Welcome to my page!</h1>' 
+
+#Anything included in the route passed to the app.route decorator with angle brackets <> surrounding it will be passed to the decorated function as a parameter. 
+@app.route('/<string:username>')
+def user(username):
+    return f'<h1>Profile for {username}</h1>' 
+
+if __name__ == '__main__':
+    app.run(port=5555, debug=True)
